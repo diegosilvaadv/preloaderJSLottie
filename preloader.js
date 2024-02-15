fcec6ad495593c7504d6bccf59adc2f4d39f874d
diegosilvaadv <91504617+diegosilvaadv@users.noreply.github.com>
2024-02-14 22:05:45 -0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -38,7 +38,7 @@ function hideOverlay() {
         renderer: 'svg', // ou 'canvas' se preferir
         loop: true,
         autoplay: true,
-        path: 'https://diegosilvaadv.github.io/preloaderJSLottie/JVGIsylS3F-3.json',
+        path: 'https://diegosilvaadv.github.io/preloaderJSLottie/simpleloading.json',
     });
 }
 
